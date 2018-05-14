52b86c860d8a33719ff4cffa89f96a63d5ecb886
Super <super@laptop-mk1l3qp1.kea.local>
2018-05-14 16:06:35 +0200

    everything okay

--- w20monday/mysolution/main.js
+++ w20monday/mysolution/main.js
@@ -1,1 +1,11 @@
 `use script`
+
+const pop = document.querySelector(`#popup`);
+const butonnn = document.querySelector(`#cookieAcceptBarConfirm`)
+window.addEventListener('load', function() {
+    pop.style.transform = `translateY(-30vh) `;
+});
+
+butonnn.addEventListener('click', function() {
+    pop.style.transform = `translateY(30vh) `;
+});
